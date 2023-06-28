d402bb21172bdce3edeefefd16363384d30b8c00
higorini <108697425+higorini@users.noreply.github.com>
2023-06-28 20:29:53 -0300

    Banner - Estados - Busca
    - Parte do Banner feita
    - Parte da Busca feita
    - Estados concluído

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect, createContext } from "react";
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+import { housesData } from "../data";
+
+export const HouseContext = createContext();
+
+const HouseContextProvider = ({ children }) => {
+  const [houses, setHouses] = useState(housesData);
+  const [country, setCountry] = useState("Estado (qualquer)");
+  const [countries, setCountries] = useState([]);
+  const [property, setProperty] = useState("Tipo (qualquer)");
+  const [properties, setProperties] = useState([]);
+  const [price, setPrice] = useState("Preço (qualquer)");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const allCountries = houses.map((house) => {
+      return house.country;
+    });
+
+    const uniqueCountries = ["Estado (qualquer)", ...new Set(allCountries)];
+
+    setCountries(uniqueCountries);
+  });
+
+  return (
+    <HouseContext.Provider
+      value={{
+        country,
+        setCountry,
+        countries,
+        property,
+        setProperty,
+        properties,
+        price,
+        setPrice,
+        houses,
+        loading,
+      }}
+    >
+      {children}
+    </HouseContext.Provider>
+  );
 };
 
-export default HouseContext;
+export default HouseContextProvider;
